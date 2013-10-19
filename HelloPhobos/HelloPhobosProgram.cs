9de289e0b131e988a6609205d1dd1a521be760c8
tgijsola <jvsola@gmail.com>
2013-10-19 16:55:27 -0700

    First project check-in

---
+++ HelloPhobos/HelloPhobosProgram.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace HelloPhobos
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class HelloPhobosProgram
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new HelloPhobos())
+                game.Run();
+        }
+    }
+#endif
+}
